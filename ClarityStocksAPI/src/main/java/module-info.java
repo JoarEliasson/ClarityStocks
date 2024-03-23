b213898db766d0dc7256dd802a287cb6836c71e8
olivia svensson <oliviateoliva@gmail.com>
2024-03-23 19:34:48 +0100

    add comments to module-info.java

--- ClarityStocksAPI/src/main/java/module-info.java
+++ ClarityStocksAPI/src/main/java/module-info.java
@@ -1,3 +1,9 @@
+/**
+ * Descriptor file. Specifies requirements and exports of module ClarityStocksAPI. Declares dependencies for this module. Descrines accessible packages to other modules.
+ * requires - means this module requires a certain module
+ * exports - certain package is exported by this module
+ * */
+
 module ClarityStocksAPI {
     requires java.net.http;
     requires com.fasterxml.jackson.databind;
