61efbfd3de80799673c485ef9982730b82112135
Douglas <doggeat@gmail.com>
2024-03-25 16:10:32 +0100

    Linegraph is kinda broken, trying to get it to work on two threads

--- ClarityStocksGUI/src/main/java/SE/ClarityStocksGUI/controller/graphControllers/GUIStockLinegraphController.java
+++ ClarityStocksGUI/src/main/java/SE/ClarityStocksGUI/controller/graphControllers/GUIStockLinegraphController.java
@@ -5,25 +5,30 @@ import SE.ClarityStocksGUI.view.GUIMainApplication;
 import alphaVantage.AlphaVantageClient;
 import alphaVantage.DataPoint;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Tooltip;
+import javafx.util.StringConverter;
 
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class GUIStockLinegraphController {
     @FXML
-    private LineChart<String, Number> chart;
+    private LineChart<Number, Number> chart;
     @FXML
-    private CategoryAxis xAxis;
+    private NumberAxis xAxis;
     @FXML
     private NumberAxis yAxis;
     private GUIMainApplication application;
     private AlphaVantageClient alphaVantageClient;
     private static GUIStockLinegraphController stockLinegraphController;
+    private List<DataPoint> list;
 
     public void initialize(){
         stockLinegraphController = this;
@@ -37,26 +42,56 @@ public class GUIStockLinegraphController {
     }
 
     public void loadStockData(){
-        XYChart.Series<String, Number> series = new XYChart.Series<>();
-        List<DataPoint> list = alphaVantageClient.getFilteredSeries();
-        for(DataPoint data : list){
-            XYChart.Data<String, Number> point = new XYChart.Data<>(data.getDate(), data.getClose());
-            series.getData().add(point);
-
-
-            /* TODO ADD TOOLTIP
-            Tooltip tooltip = new Tooltip("Closing price: " + data.getClose());
-            Tooltip.install(point.getNode(), tooltip);
-
-             */
+        if(!(chart.getData().isEmpty())){
+            chart.getData().clear();
         }
+        xAxis.setForceZeroInRange(false);
 
-        chart.getData().add(series);
+
+
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+
+                list = alphaVantageClient.getFilteredSeries();
+                populateChart();
+
+                    /* TODO ADD TOOLTIP
+                    Tooltip tooltip = new Tooltip("Closing price: " + data.getClose());
+                    Tooltip.install(point.getNode(), tooltip);
+
+                     */
+
+            }
+        });
+
 
     }
 
     public static GUIStockLinegraphController getInstance(){
         return stockLinegraphController;
+    }
+
+    public void populateChart(){
+        XYChart.Series<Number, Number> series = new XYChart.Series<>();
+
+
+        for (DataPoint data : list) {
+            String[] date = data.getDate().split("-");
+            StringBuilder dateString = new StringBuilder();
+            for(String s : date){
+                dateString.append(s);
+            }
+
+
+            System.out.println(dateString);
+
+
+            XYChart.Data<Number, Number> point = new XYChart.Data<>(Integer.parseInt(dateString.toString()), data.getClose());
+            series.getData().add(point);
+        }
+
+        chart.getData().add(series);
     }
 
 }
