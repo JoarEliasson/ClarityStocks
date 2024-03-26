447110a4ce5b0e10b69ff211da5517d95362bae4
olivia svensson <oliviateoliva@gmail.com>
2024-03-26 17:14:48 +0100

    fix error

--- ClarityStocksAPI/src/main/java/yahooFinance/StockQuotesQuery1V7Request.java
+++ ClarityStocksAPI/src/main/java/yahooFinance/StockQuotesQuery1V7Request.java
@@ -2,12 +2,13 @@ package yahooFinance;
 
 public class StockQuotesQuery1V7Request {
     String symbol;
+    Stock stock;
 
     public StockQuotesQuery1V7Request(String symbol) {
         this.symbol = symbol;
     }
 
     public Stock getSingleResult() {
-
+        return stock;
     }
 }
