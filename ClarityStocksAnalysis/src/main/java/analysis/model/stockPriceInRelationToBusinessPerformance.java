fcd3f9f5ada6cd5f7fc88c0b3f295af713604c06
olivia svensson <oliviateoliva@gmail.com>
2024-04-13 08:14:24 +0200

    Create stockPriceInRelationToBusinessPerformance.java

---
+++ ClarityStocksAnalysis/src/main/java/analysis/model/stockPriceInRelationToBusinessPerformance.java
@@ -0,0 +1,10 @@
+package analysis.model;
+/** Class for evaluating the stock price in relation to how the company is performing.
+ * How is the anual earnings per share related to share price. P/E-ratio.
+ * @author Olivia Svensson
+ * */
+public class stockPriceInRelationToBusinessPerformance
+{
+    
+
+}
