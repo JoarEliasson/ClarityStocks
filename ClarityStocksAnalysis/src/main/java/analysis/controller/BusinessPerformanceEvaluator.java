230d11cdc2776e32705ad2dc2ca6693c00d1633b
olivia svensson <oliviateoliva@gmail.com>
2024-04-13 07:47:56 +0200

    create businessperformanceevaluator class

--- ClarityStocksAnalysis/src/main/java/analysis/controller/BusinessPerformanceEvaluator.java
+++ ClarityStocksAnalysis/src/main/java/analysis/controller/BusinessPerformanceEvaluator.java
@@ -1,4 +1,19 @@
 package analysis.controller;
+import analysis.model.BusinessPerformanceEvaluation;
 
 public class BusinessPerformanceEvaluator {
+
+    public BusinessPerformanceEvaluator (String symbol, double ebidta, double totalRevenue)
+
+    {
+        businessPerformanceEvaluation(symbol, ebidta, totalRevenue);
+    }
+
+    public BusinessPerformanceEvaluation businessPerformanceEvaluation(String symbol, double ebidta, double totalRevenue) {
+        BusinessPerformanceEvaluation businessPerformanceEvaluation =  new BusinessPerformanceEvaluation(symbol, ebidta, totalRevenue);
+        return businessPerformanceEvaluation;
+    }
+    public String getDescription(String symbol, double ebidta, double totalRevenue) {
+       return  businessPerformanceEvaluation(symbol, ebidta, totalRevenue).getDescription();
+    }
 }
