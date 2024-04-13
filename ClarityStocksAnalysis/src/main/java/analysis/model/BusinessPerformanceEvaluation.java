f33bf5f19ad558ff60e2b1780bc2eb629abe55a3
olivia svensson <oliviateoliva@gmail.com>
2024-04-13 07:27:11 +0200

    add businessperformanceevaluation

--- ClarityStocksAnalysis/src/main/java/analysis/model/BusinessPerformanceEvaluation.java
+++ ClarityStocksAnalysis/src/main/java/analysis/model/BusinessPerformanceEvaluation.java
@@ -1,4 +1,35 @@
 package analysis.model;
 
+/**Class for calculating the business performance of a company. Returns an EBITDA margin.
+ * EBIDTA margin is calculated by EBIDTA divided by the companies total revenue.
+ * @author Olivia Svensson
+ * */
 public class BusinessPerformanceEvaluation {
+    private final String symbol;
+    private final double ebidtamargin;
+    private final String description;
+
+
+    public BusinessPerformanceEvaluation(String symbol, double ebidta, double totalRevenue) {
+        this.symbol = symbol;
+        ebidtamargin =  calcEBIDTAMargin(ebidta, totalRevenue);
+        description =  evaluateEBIDTAMargin(ebidtamargin);
+
+    }
+
+    private double calcEBIDTAMargin(double ebidta, double totalRevenue) {
+        return ebidta/totalRevenue;
+    }
+
+    private String evaluateEBIDTAMargin(double ebidtamargin) {
+        if(ebidtamargin < 0.05) {
+            return  "There is an indication that " + symbol + " has a weak performance";
+        } else if (ebidtamargin > 0.05 && ebidtamargin < 0.15) {
+            return  "There is an indication that " + symbol + " has an average performance";
+        } else if (ebidtamargin > 0.15 && ebidtamargin < 0.20) {
+            return  "There is an indication that " + symbol + " has a good performance";
+        } else {
+            return  "There is an indication that " + symbol + " has a great performance";
+        }
+    }
 }
