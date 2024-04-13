222a138e81adba2615b5f3990238d8c292dcc920
olivia svensson <oliviateoliva@gmail.com>
2024-04-13 08:16:18 +0200

    Update stockPriceInRelationToBusinessPerformance.java

--- ClarityStocksAnalysis/src/main/java/analysis/model/stockPriceInRelationToBusinessPerformance.java
+++ ClarityStocksAnalysis/src/main/java/analysis/model/stockPriceInRelationToBusinessPerformance.java
@@ -1,10 +1,11 @@
 package analysis.model;
 /** Class for evaluating the stock price in relation to how the company is performing.
- * How is the anual earnings per share related to share price. P/E-ratio.
+ * How is the annual earnings per share related to share price. P/E-ratio.
+ * Varies across sectors. The P/E-ratio is not always an accurate indicator for if the company's performance.
  * @author Olivia Svensson
  * */
 public class stockPriceInRelationToBusinessPerformance
 {
-    
+
 
 }
