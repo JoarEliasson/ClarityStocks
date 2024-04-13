2e09baad2ca24fe3db378f66e93fb117f83ee7cd
olivia svensson <oliviateoliva@gmail.com>
2024-04-13 16:49:26 +0200

    Create AnalystPrediction.java

---
+++ ClarityStocksAnalysis/src/main/java/analysis/model/AnalystPrediction.java
@@ -0,0 +1,11 @@
+package analysis.model;
+
+/** Class for evaluating the analyst prediction of a stock
+ * @author Olivia Svensson
+ * */
+public class AnalystPrediction {
+    String symbol;
+    String description;
+
+
+}
