ee7900d63ef334098f7f577504c07015f024f867
olivia svensson <oliviateoliva@gmail.com>
2024-04-26 15:13:19 +0200

    Update MultipleRegression.java

--- ClarityStocksAnalysis/src/main/java/analysis/regression/MultipleRegression.java
+++ ClarityStocksAnalysis/src/main/java/analysis/regression/MultipleRegression.java
@@ -1,10 +1,11 @@
 package analysis.regression;
 
 /**
- * Class for implementing a linear regression. Linear regression is used for predicting the future price of the stock
- * by analyzing historical data. Uses an X and Y value which are data pairs. Model implements Pearson's linear
- * regression.
+ * Class for implementing a multiple linear regression. Multiple linear regression is used for predicting the future
+ * price of the stock by analyzing historical data. Uses an X and Y value which are data pairs. Model implements //add
+ * which kind of MLR is implemented here.
  * @author Olivia Svensson
  * */
 public class MultipleRegression {
+
 }
