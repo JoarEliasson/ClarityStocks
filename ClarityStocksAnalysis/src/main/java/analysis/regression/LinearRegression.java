86ee0d2537c26478d21bc1befd7e0cfb1e009f9c
olivia svensson <oliviateoliva@gmail.com>
2024-04-26 15:11:42 +0200

    create multiple linear regression class, change datapoints

--- ClarityStocksAnalysis/src/main/java/analysis/regression/LinearRegression.java
+++ ClarityStocksAnalysis/src/main/java/analysis/regression/LinearRegression.java
@@ -1,26 +1,27 @@
 package analysis.regression;
 
 import alphaVantage.model.data.series.DailyDataPoint;
-
+import org.apache.commons.math3.stat.regression.SimpleRegression;
 /**
  * Class for implementing a linear regression. Linear regression is used for predicting the future price of the stock
- * by analyzing historical data. Uses an X and Y value which are data pairs. Model implements Pearsons linear regression
- *
+ * by analyzing historical data. Uses an X and Y value which are data pairs. Model implements Pearson's linear
+ * regression.
  * @author Olivia Svensson
  * */
 public class LinearRegression {
     String symbol;
-    DailyDataPoint[] dailyDataPoints;
+    double[][] dailyDataPoints;
+    SimpleRegression simpleRegression;
 
-    public LinearRegression(String symbol, DailyDataPoint[] dailyDataPoints) {
+    public LinearRegression(String symbol, double[][] dailyDataPoints) {
         this.symbol = symbol;
         this.dailyDataPoints = dailyDataPoints;
     }
 
 
     private void linearRegression() {
-        
-
+        simpleRegression = new SimpleRegression(true);
+        simpleRegression.addData(dailyDataPoints);
     }
 
 }
---
+++ ClarityStocksAnalysis/src/main/java/analysis/regression/MultipleRegression.java
@@ -0,0 +1,10 @@
+package analysis.regression;
+
+/**
+ * Class for implementing a linear regression. Linear regression is used for predicting the future price of the stock
+ * by analyzing historical data. Uses an X and Y value which are data pairs. Model implements Pearson's linear
+ * regression.
+ * @author Olivia Svensson
+ * */
+public class MultipleRegression {
+}
