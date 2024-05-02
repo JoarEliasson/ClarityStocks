7459fd9f65c450410e6317932a76582bae90aedc
olivia svensson <oliviateoliva@gmail.com>
2024-05-02 16:49:00 +0200

    Update LinearRegressionAnalysis.java

--- ClarityStocksAPI/src/main/java/alphaVantage/regression/LinearRegressionAnalysis.java
+++ ClarityStocksAPI/src/main/java/alphaVantage/regression/LinearRegressionAnalysis.java
@@ -17,7 +17,7 @@ public class LinearRegressionAnalysis {
   private IncomeStatement incomeStatement;
   private AlphaVantageClient alphaVantageClient;
   private String symbol;
-  private long[] incomeStatementsList;
+  private ArrayList<Long> incomeStatementsList;
   private int numberOfIncomeStatements;
   private List<String> results = new ArrayList<>();
 
@@ -26,21 +26,54 @@ public class LinearRegressionAnalysis {
     this.symbol = symbol;
     this.alphaVantageClient = alphaVantageClient;
     numberOfIncomeStatements = 24;
-    incomeStatementsList = new long[numberOfIncomeStatements];
+    incomeStatementsList = new ArrayList<Long>(numberOfIncomeStatements);
 
     //amount of statements in IncomeStatement class which are able to run the analysis on
     linearRegressions = new LinearRegression[numberOfIncomeStatements];
   }
 
+  /**
+   * Method which populates the list of income statements with income statements from the stock company.
+   * The arraylist indexes are ordered in the same way that the variables in the class IncomeStatement are declared.
+   * */
   private void populateIncomeStatementList() {
-    incom
+    incomeStatementsList.set(0,incomeStatement.getGrossProfit());
+    incomeStatementsList.set(1,incomeStatement.getTotalRevenue());
+    incomeStatementsList.set(2, incomeStatement.getCostOfRevenue());
+    incomeStatementsList.set(3, incomeStatement.getCostofGoodsAndServicesSold());
+    incomeStatementsList.set(4, incomeStatement.getOperatingIncome());
+    incomeStatementsList.set(5, incomeStatement.getSellingGeneralAndAdministrative());
+    incomeStatementsList.set(6, incomeStatement.getResearchAndDevelopment());
+    incomeStatementsList.set(7, incomeStatement.getOperatingExpenses());
+    incomeStatementsList.set(8, incomeStatement.getInvestmentIncomeNet());
+    incomeStatementsList.set(9, incomeStatement.getInterestIncome());
+    incomeStatementsList.set(10, incomeStatement.getInterestExpense());
+    incomeStatementsList.set(11, incomeStatement.getNonInterestIncome());
+    incomeStatementsList.set(12, incomeStatement.getOtherNonOperatingIncome());
+    incomeStatementsList.set(13, incomeStatement.getDepreciation());
+    incomeStatementsList.set(14, incomeStatement.getDepreciationAndAmortization());
+    incomeStatementsList.set(15, incomeStatement.getIncomeBeforeTax());
+    incomeStatementsList.set(16, incomeStatement.getIncomeTaxExpense());
+    incomeStatementsList.set(17, incomeStatement.getInterestAndDebtExpense());
+    incomeStatementsList.set(18, incomeStatement.getNetIncomeFromContinuingOperations());
+    incomeStatementsList.set(19, incomeStatement.getEbit());
+    incomeStatementsList.set(20, incomeStatement.getEbitda());
+    incomeStatementsList.set(21, incomeStatement.getNetIncome());
+  }
 
-   }
-
+  /**
+   * Method for getting the array of linear regressions made of all the income statements.
+   * */
   public LinearRegression[] getLinearRegressions() {
     return this.linearRegressions;
   }
 
+
+  /**
+   * Method for making all the linear regressions based on the income statements.
+   * The method has a for-loop which loops through all the elements of the linear regression array. For each element
+   * the method assigns the element in the array to a new linear regression made.
+   * */
   private void makeLinearRegressions() {
     for(int i = 0; i < linearRegressions.length; i++) {
       linearRegressions[i] = new LinearRegression(symbol, );
