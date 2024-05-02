68090316102cda5093ef496612ca38e575710928
olivia svensson <oliviateoliva@gmail.com>
2024-05-02 16:23:48 +0200

    Update LinearRegressionAnalysis.java

--- ClarityStocksAPI/src/main/java/alphaVantage/regression/LinearRegressionAnalysis.java
+++ ClarityStocksAPI/src/main/java/alphaVantage/regression/LinearRegressionAnalysis.java
@@ -1,5 +1,8 @@
 package alphaVantage.regression;
 
+import alphaVantage.controller.AlphaVantageClient;
+import alphaVantage.model.data.fundamental.IncomeStatement;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,7 +13,38 @@ import java.util.List;
  * */
 
 public class LinearRegressionAnalysis {
+  private LinearRegression[] linearRegressions;
+  private IncomeStatement incomeStatement;
+  private AlphaVantageClient alphaVantageClient;
+  private String symbol;
+  private long[] incomeStatementsList;
+  private int numberOfIncomeStatements;
+  private List<String> results = new ArrayList<>();
 
-  List<String> results = new ArrayList<>();
+  public LinearRegressionAnalysis(IncomeStatement incomeStatement, String symbol, AlphaVantageClient alphaVantageClient){
+    this.incomeStatement = incomeStatement;
+    this.symbol = symbol;
+    this.alphaVantageClient = alphaVantageClient;
+    numberOfIncomeStatements = 24;
+    incomeStatementsList = new long[numberOfIncomeStatements];
+
+    //amount of statements in IncomeStatement class which are able to run the analysis on
+    linearRegressions = new LinearRegression[numberOfIncomeStatements];
+  }
+
+  private void populateIncomeStatementList() {
+    incom
+
+   }
+
+  public LinearRegression[] getLinearRegressions() {
+    return this.linearRegressions;
+  }
+
+  private void makeLinearRegressions() {
+    for(int i = 0; i < linearRegressions.length; i++) {
+      linearRegressions[i] = new LinearRegression(symbol, );
+    }
+  }
 
 }
