c7f01a5f1fa4f75f0e05d10411dcaa3193539b0f
JoarEliasson <joar.eliasson@gmail.com>
2024-05-25 13:23:07 +0200

    Updated RegressionTile
    Signed-off-by: JoarEliasson <joar.eliasson@gmail.com>

--- clarity-stocks-gui/src/main/java/com/claritystocks/controller/tiles/RegressionTile.java
+++ clarity-stocks-gui/src/main/java/com/claritystocks/controller/tiles/RegressionTile.java
@@ -2,17 +2,18 @@ package com.claritystocks.controller.tiles;
 
 import analysis.regression.RegressionAnalysis;
 import analysis.regression.RegressionResult;
-import common.data.series.DailyDataPoint;
 import eu.hansolo.tilesfx.Tile;
 import eu.hansolo.tilesfx.Tile.SkinType;
-import eu.hansolo.tilesfx.Tile.TileColor;
+import eu.hansolo.tilesfx.Tile.TextSize;
 import eu.hansolo.tilesfx.TileBuilder;
 import eu.hansolo.tilesfx.chart.ChartData;
+import eu.hansolo.tilesfx.colors.Medium;
 import java.util.List;
 import javafx.fxml.FXML;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Stop;
 import model.stock.StockData;
 
 public class RegressionTile {
@@ -51,10 +52,11 @@ public class RegressionTile {
   }
 
   private void createRadialChart(List<RegressionResult> topResults) {
+    String date = topResults.getFirst().getLatestPrediction().getPredictionDate();
     radialChartTile = TileBuilder.create()
         .skinType(SkinType.RADIAL_CHART)
-        .title("Regression Analysis")
-        .text("Top 3 R-squared values")
+        .title("Regression Analysis Top 3 R-Squared")
+        .text("Actual price vs. Predicted price " + date)
         .chartData(createChartData(topResults.get(0), 0), createChartData(topResults.get(1), 1), createChartData(topResults.get(2), 2))
         .animated(true)
         .backgroundColor(Color.TRANSPARENT)
@@ -64,7 +66,7 @@ public class RegressionTile {
         .descriptionColor(Color.BLACK)
         .textColor(Color.BLACK)
         .needleColor(Color.BLACK)
-        .prefSize(400, 400)
+        .prefSize(300, 300)
         .build();
     radialTileContainer.getChildren().add(radialChartTile);
   }
@@ -94,16 +96,16 @@ public class RegressionTile {
   private Tile createGaugeTile(RegressionResult result) {
     double rating = result.getLatestPrediction().getRating();
     String variableName = result.getVariable();
-    String date = result.getLatestPrediction().getPredictionDate();
+
     Color ratingColor = getRatingColor(rating);
 
     return TileBuilder.create()
         .skinType(SkinType.GAUGE2)
-        .prefSize(200, 200)
+        .prefSize(250, 220)
         .title(variableName)
-        .text("Latest Prediction")
+        .text("of prediction")
         .unit("%")
-        .description("Date: " + date)
+        .textSize(TextSize.BIGGER)
         .maxValue(200)
         .minValue(0)
         .value(rating * 100)
@@ -115,17 +117,43 @@ public class RegressionTile {
         .descriptionColor(Color.BLACK)
         .textColor(Color.BLACK)
         .needleColor(Color.BLACK)
+        .gradientStops(
+            new Stop(0.0, Medium.RED),
+            new Stop(0.8, Medium.ORANGE_RED),
+            new Stop(1, Medium.ORANGE),
+            new Stop(1.05, Medium.YELLOW_ORANGE),
+            new Stop(1.10, Medium.YELLOW),
+            new Stop(1.15, Medium.GREEN_YELLOW),
+            new Stop(1.3, Medium.GREEN),
+            new Stop(1.5, Medium.BLUE_GREEN))
+        .strokeWithGradient(true)
         .animated(true)
         .build();
   }
 
   private Color getRatingColor(double rating) {
-    if (rating < 0.95) {
-      return Color.RED;
-    } else if (rating < 1.025) {
-      return Color.YELLOW;
-    } else {
+    if (rating < 0.90) {
+      return Color.DARKSEAGREEN;
+    } else if (rating < 0.95) {
       return Color.GREEN;
+    } else if (rating < 0.975) {
+      return Color.LIGHTGREEN;
+    } else if (rating < 1.0) {
+      return Color.GREENYELLOW;
+    } else if (rating < 1.025) {
+      return Color.LIGHTYELLOW;
+    } else if (rating < 1.05) {
+      return Color.YELLOW;
+    } else if (rating < 1.075) {
+      return Color.GOLD;
+    } else if (rating < 1.1) {
+      return Color.ORANGE;
+    } else if (rating < 1.15) {
+      return Color.DARKORANGE;
+    } else if (rating < 1.2) {
+      return Color.RED;
+    } else {
+      return Color.DARKRED;
     }
   }
 
